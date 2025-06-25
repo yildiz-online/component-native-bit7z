0488858f099fe89d865dcc8c0c0a0dde29056f6a
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2025-06-25 07:05:17 +0200

    [WIP] Initial commit.

---
+++ src/main/scripts/win64/build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+cmake ../../c++ \
+-DCMAKE_BUILD_TYPE=Release \
+-DCMAKE_INSTALL_PREFIX="../../../../target/classes/win64" \
+-DCMAKE_TOOLCHAIN_FILE=mingw-toolchain.cmake
+
+make install
+r1=$?
+
+exit $r1
