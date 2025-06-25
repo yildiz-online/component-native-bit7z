5710ff3c78f873f00d4f6e41bd01d61bab14ca13
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2025-06-25 09:49:37 +0200

    [WIP] Set 7z to 24.09.

--- src/main/scripts/win64/build.sh
+++ src/main/scripts/win64/build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 cmake ../../c++ \
+-DBIT7Z_7ZIP_VERSION="24.09" \
 -DCMAKE_BUILD_TYPE=Release \
 -DCMAKE_INSTALL_PREFIX="../../../../target/classes/win64" \
 -DCMAKE_TOOLCHAIN_FILE=mingw-toolchain.cmake
